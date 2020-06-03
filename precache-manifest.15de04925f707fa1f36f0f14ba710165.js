32fe03f7301c83fc531e735bcafb5209659da209
ANKIT <mr.ankityo111@gmail.com>
2020-06-04 05:05:55 +0530

    Updates

---
+++ precache-manifest.15de04925f707fa1f36f0f14ba710165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963dc12241184ef4bdbe582185f109b8",
+    "url": "/rY/index.html"
+  },
+  {
+    "revision": "c3a628df0aad78579897",
+    "url": "/rY/static/css/main.58167424.chunk.css"
+  },
+  {
+    "revision": "822764a1d53898d8ae86",
+    "url": "/rY/static/js/2.c9675d40.chunk.js"
+  },
+  {
+    "revision": "40897c1737561d02b0d02fd516ba3f24",
+    "url": "/rY/static/js/2.c9675d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a628df0aad78579897",
+    "url": "/rY/static/js/main.d684e80f.chunk.js"
+  },
+  {
+    "revision": "d80dd6b4546af6758ecf",
+    "url": "/rY/static/js/runtime-main.01ca9bcf.js"
+  }
+]);
